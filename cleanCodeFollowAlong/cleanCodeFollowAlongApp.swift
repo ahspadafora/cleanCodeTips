9043a2788c206030023fa3ed773d0c4b3119758f
Amber Spadafora <ahelenespadafora@gmail.com>
2022-01-29 16:49:54 -0500

    Initial Commit

---
+++ cleanCodeFollowAlong/cleanCodeFollowAlongApp.swift
@@ -0,0 +1,17 @@
+//
+//  cleanCodeFollowAlongApp.swift
+//  cleanCodeFollowAlong
+//
+//  Created by Amber Spadafora on 1/29/22.
+//
+
+import SwiftUI
+
+@main
+struct cleanCodeFollowAlongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
